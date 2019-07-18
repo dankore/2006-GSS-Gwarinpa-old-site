0d987372d55e5729537c106b57b2eaacc89f17f7
Adamu Muhammad Dankore <45286534+dankore@users.noreply.github.com>
2019-07-18 02:51:38 -0500

    Initial set up and running

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import Handlebars from "handlebars";
+// import Handlebars from "handlebars"
+
 const request = new XMLHttpRequest();
 
 request.open("GET", "https://dankore.github.io/gss-2006-json/2006.json", true);
@@ -9,11 +12,65 @@ request.onload = () => {
 request.send();
 
 //Render JSON data to browser
-const jsonContainer = document.querySelector(".json-container");
+
 const render = myData => {
-  const html = myData.set.map(item => {
-    return " " + item.name;
-  });
-  console.log(myData);
-  jsonContainer.innerHTML = html;
+  const handleBarTemplate = document.querySelector("#template").innerHTML;
+
+  const compiled = Handlebars.compile(handleBarTemplate);
+  const generatedHtml = compiled(myData);
+  const jsonContainerinHtml = document.querySelector(".json-container");
+
+  jsonContainerinHtml.innerHTML = generatedHtml;
 };
+
+// Calculate number of days
+Handlebars.registerHelper("calculateUntillBirthDay", dob => {
+  const daysIntoYear = date => {
+    return (
+      (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) -
+        Date.UTC(date.getFullYear(), 0, 0)) /
+      24 /
+      60 /
+      60 /
+      1000
+    );
+  };
+
+  //Get input
+  const arr = dob.split(" ");
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  const arr1 = arr[0];
+  const arr2 = parseInt(arr[1], 10);
+
+  const monthStr = months.indexOf(arr1);
+
+  const currentDate = new Date();
+  const futureDate = new Date(0, monthStr, arr2);
+  const futureDays = daysIntoYear(futureDate);
+  const numberOfDaysToDate = daysIntoYear(currentDate);
+
+  const daysRemaining = futureDays - numberOfDaysToDate;
+
+  if (daysRemaining < 0) {
+    return daysRemaining + 365 + " days to birthday";
+  } else if (daysRemaining === 0) {
+    return "Today is birthday 🎂";
+  } else {
+    return daysRemaining + " days to birthday";
+  }
+});
